2e6b288d14753cbf56c4fcc782f351715e1f049b
Lucas Candido <lucascomercial64@gmail.com>
2026-02-25 11:06:44 +0000

    File changes

--- src/pages/PlanView.jsx
+++ src/pages/PlanView.jsx
@@ -171,7 +171,7 @@ export default function PlanView() {
       </div>
 
       {/* Content */}
-      <div id="plan-print-area" className="max-w-4xl mx-auto px-4 md:px-8 py-8">
+      <div id="plan-print-area" className="mx-auto px-4 md:px-8 py-8" style={{ maxWidth: "900px" }}>
         <PlanDocument plan={plan} patientData={patientData} />
       </div>
 
