4792c29b60ef7f0b531778b00a407c3f5f203f2a
Lucas Candido <lucascomercial64@gmail.com>
2026-02-25 11:09:07 +0000

    File changes

--- src/components/plan/PlanDocument.jsx
+++ src/components/plan/PlanDocument.jsx
@@ -186,7 +186,8 @@ export default function PlanDocument({ plan, patientData }) {
       {/* ============ PAGE 2+ - Etapas ============ */}
       {etapas.map((etapa, ei) => (
         <div key={ei} id={`plan-page-etapa-${ei + 1}`} style={pageStyle}>
-          <img src={FOOTER_IMAGE_URL} alt="" style={{ position: "absolute", bottom: 0, left: 0, width: "100%", display: "block" }} />
+          <img src={BG_IMAGE_URL} alt="" style={bgStyle} />
+          <img src={FOOTER_IMAGE_URL} alt="" style={{ position: "absolute", bottom: 0, left: 0, width: "100%", display: "block", zIndex: 0 }} />
           <div style={contentStyle}>
 
             <div style={{ textAlign: "center", marginBottom: 12 }}>
