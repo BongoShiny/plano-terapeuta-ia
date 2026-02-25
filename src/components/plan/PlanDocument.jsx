63e7404533a3c1d65b59684d7e8d93474f0f32da
Lucas Candido <lucascomercial64@gmail.com>
2026-02-25 11:38:06 +0000

    File changes

--- src/components/plan/PlanDocument.jsx
+++ src/components/plan/PlanDocument.jsx
@@ -163,7 +163,7 @@ export default function PlanDocument({ plan, patientData }) {
             <>
               <div style={{ marginBottom: 14 }}>
                 <SectionTitle>Objetivo Geral do Tratamento</SectionTitle>
-                <p style={{ fontSize: 10.5, lineHeight: 1.7, margin: 0, paddingLeft: 8, textAlign: "justify" }}>
+                <p style={{ fontSize: 12.5, lineHeight: 1.7, margin: 0, paddingLeft: 8, textAlign: "justify" }}>
                   {planData.objetivo_geral}
                 </p>
               </div>
